627693ed32326b9c643d84fcb96249a32fa62bf1
manish35809 <manishchoudhary35809@gmail.com>
2020-03-27 15:04:23 +0530

    updated-Heading

--- src/Heading.js
+++ src/Heading.js
@@ -5,7 +5,7 @@ import "./layout.css"
 export default function Heading() {
     return (
     <h1 className="text-center border rounded border-success p-1 text-light mt-4 mb-4">
-        My ToDo App
+        My Notes
     </h1>
     )
 }
