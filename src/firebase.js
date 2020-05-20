693b6620efe6f2ca5c6c31327677549f32fda30d
Manish Choudhary <46993442+manish35809@users.noreply.github.com>
2020-05-20 20:14:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDl4mtJRp-mewfTHfQMbsfjll2y1eUfdFw",
-  authDomain: "basic-cafe-fad15.firebaseapp.com",
-  databaseURL: "https://basic-cafe-fad15.firebaseio.com",
-  projectId: "basic-cafe-fad15",
-  storageBucket: "basic-cafe-fad15.appspot.com",
-  messagingSenderId: "1090987515710",
-  appId: "1:1090987515710:web:cd6a14455d22764c7ad66f",
-  measurementId: "G-JVGRLM0WB4"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
